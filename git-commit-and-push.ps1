b5217da8aad1b9f738c043dee798be9545cb7027
Ben K <team.cryptoworx@gmail.com>
2023-04-17 19:29:36 +0100

    [Ben K] Automated Documentation Update

---
+++ git-commit-and-push.ps1
@@ -0,0 +1,7 @@
+
+Write-Host "adding changes to git (git add .)"
+git add .
+Write-Host "committing changes to git (git commit -m [Ben K] Automated Documentation Update)"
+git commit -m "[Ben K] Automated Documentation Update"
+Write-Host "pushing changes to git (git push)"
+git push
